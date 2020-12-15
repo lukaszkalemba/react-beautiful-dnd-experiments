680f6714a6b8da6c88146250662bb8c6e48c32e7
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-12-15 11:10:58 +0100

    Create list and use DragDropContext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+const items = [
+  { id: 1, content: 'Lorem ipsum' },
+  { id: 2, content: 'Lorem ipsum' },
+  { id: 3, content: 'Lorem ipsum' },
+  { id: 4, content: 'Lorem ipsum' },
+  { id: 5, content: 'Lorem ipsum' },
+  { id: 6, content: 'Lorem ipsum' },
+  { id: 7, content: 'Lorem ipsum' },
+  { id: 8, content: 'Lorem ipsum' },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <DragDropContext onDragEnd={() => {}}>
+        <div>
+          {items.map((item) => (
+            <div
+              style={{
+                padding: '10px 20px',
+                margin: '5px auto',
+                border: '1px solid black',
+                maxWidth: '400px',
+              }}
+              key={item.id}
+            >
+              {item.content}
+            </div>
+          ))}
+        </div>
+      </DragDropContext>
     </div>
   );
 }
