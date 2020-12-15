4760d62935eb3495ce9d38aa5a2a4ccfab845b8d
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-12-15 11:44:46 +0100

    Update initial state

--- src/App.js
+++ src/App.js
@@ -1,44 +1,100 @@
-import './App.css';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-const items = [
-  { id: 1, content: 'Lorem ipsum' },
-  { id: 2, content: 'Lorem ipsum' },
-  { id: 3, content: 'Lorem ipsum' },
-  { id: 4, content: 'Lorem ipsum' },
-  { id: 5, content: 'Lorem ipsum' },
-  { id: 6, content: 'Lorem ipsum' },
-  { id: 7, content: 'Lorem ipsum' },
-  { id: 8, content: 'Lorem ipsum' },
+const initial = [
+  {
+    id: '1',
+    content: 'Lorem ipsum',
+  },
+  {
+    id: '2',
+    content: 'Lorem ipsum',
+  },
+  {
+    id: '3',
+    content: 'Lorem ipsum',
+  },
+  {
+    id: '4',
+    content: 'Lorem ipsum',
+  },
+  {
+    id: '5',
+    content: 'Lorem ipsum',
+  },
 ];
 
-function App() {
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const QuoteItem = styled.div`
+  width: 200px;
+  border: 1px solid grey;
+  margin-bottom: 8px;
+  background-color: lightblue;
+  padding: 8px;
+`;
+
+function Quote({ quote, index }) {
   return (
-    <div className='App'>
-      <DragDropContext onDragEnd={() => {}}>
-        <Droppable droppableId='list'>
-          {(provided) => (
-            <div ref={provided.innerRef} {...provided.droppableProps}>
-              <div>
-                {items.map((item) => (
-                  <div
-                    style={{
-                      padding: '10px 20px',
-                      margin: '5px auto',
-                      border: '1px solid black',
-                      maxWidth: '400px',
-                    }}
-                    key={item.id}
-                  >
-                    {item.content}
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-    </div>
+    <Draggable draggableId={quote.id} index={index}>
+      {(provided) => (
+        <QuoteItem
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {quote.content}
+        </QuoteItem>
+      )}
+    </Draggable>
+  );
+}
+
+const QuoteList = React.memo(function QuoteList({ quotes }) {
+  return quotes.map((quote, index) => (
+    <Quote quote={quote} index={index} key={quote.id} />
+  ));
+});
+
+function App() {
+  const [state, setState] = useState({ quotes: initial });
+
+  function onDragEnd(result) {
+    if (!result.destination) {
+      return;
+    }
+
+    if (result.destination.index === result.source.index) {
+      return;
+    }
+
+    const quotes = reorder(
+      state.quotes,
+      result.source.index,
+      result.destination.index
+    );
+
+    setState({ quotes });
+  }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='list'>
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            <QuoteList quotes={state.quotes} />
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
 
